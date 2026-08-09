3bc227f9936b142c659023f20b82e27b8bd5eb30
Faris4166 <farisarthan@gmail.com>
2026-08-09 21:29:47 +0700

    Update layout and page components for URL shortener functionality; add custom Input component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,69 +1,62 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import axios from "axios";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const [url, setUrL] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post("/shorten", { url });
+      const { short_url } = response.data;
+      setShortUrl(short_url);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleOpenUrl = () => {
+    if (shortUrl) {
+      window.open(`http://localhost:8080/${shortUrl}`, "_blank");
+    }
+  };
+  const handCopyUrl = () => {
+    if (shortUrl) {
+      navigator.clipboard.writeText(`http://localhost:8080/${shortUrl}`);
+    }
+  };
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="felx flex-col gap-2.5 justify-center items-center p-2.5">
+      <h1 className="text-4xl">Shorten URL</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2.5">
+        <Input
+          placeholder="Enter URL"
+          type="text"
+          value={url}
+          onChange={(event) => setUrL(event.target.value)}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+        <Button type="submit">Shortend</Button>
+      </form>
+      {shortUrl && (
+        <div>
+          <div>
+            <p>
+              Shortend URL:{" "}
+              <a
+                href={`http://localhost:8080/${shortUrl}`}
+                target="_black"
+                rel="noopener norferrer"
+              >{`http://localhost:8080/${shortUrl}`}</a>
+            </p>
+          </div>
+          <Button onClick={handleOpenUrl}>Open URL</Button>
+          <Button onClick={handCopyUrl}>Copy URL</Button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      )}
     </div>
   );
 }
